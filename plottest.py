39adebaf3d194f24762431729930572ca87a10c7
AlistairCoates <alistairescoates@gmail.com>
2018-04-19 17:06:28 +0100

    Test2

--- plottest.py
+++ plottest.py
@@ -2,5 +2,7 @@ import matplotlib.pyplot as plt
 import random as ran
 
 plt.plot([1,2,3],[ran.randint(1,5),ran.randint(1,5),ran.randint(1,5)],'.')
-plt.show()  
+plt.show() 
+
+print("Three random points") 
 
